8f6c8279cd266bfe9c043445471a800866a46de8
Husnil Maarif <husnilmaarif.hm@gmail.com>
2022-06-04 03:52:48 +0700

    membuat navigasi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// components
+import Navigasi from "./components/Navigasi";
+
+// pages
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigasi />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Navigasi.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown, Button } from "react-bootstrap";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import PersonIcon from "@mui/icons-material/Person";
+
+function Navigasi() {
+  return (
+    <Navbar bg="white" expand="sm" className="shadow">
+      <Container>
+        <Navbar.Brand>
+          <Link to="/">
+            <img src={logo} alt="logo" width={150} />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavDropdown title="EXPLORE" className="me-2 text-dark mt-1 link">
+              <NavDropdown.Item>
+                <Link to="/methodology" className="link">
+                  METHODOLOGY
+                </Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/valuation" className="link">
+                  VALUATION REPORT
+                </Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/data" className="link">
+                  DATA SOURCES
+                </Link>
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link className="me-2">
+              <Link to="/support" className="link">
+                SUPPORT
+              </Link>
+            </Nav.Link>
+            <Nav.Link className="me-2">
+              <Link to="/pricing" className="link">
+                PRICING
+              </Link>
+            </Nav.Link>
+            <Button className="me-2" variant="info" size="sm" id="button-buy">
+              <Link to="/buy" className="link text-light">
+                BUY NOW
+              </Link>
+            </Button>
+            <Nav.Link>
+              <Link to="/login" className="link">
+                <PersonIcon />
+              </Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigasi;
